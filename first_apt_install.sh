1a414066dd13b6293a8c41b833a06b08c0863f8b
코딩너무어렵 <131563265+youghwjdkill1@users.noreply.github.com>
2025-06-03 23:26:54 +0900

    Add files via upload

--- first_apt_install.sh
+++ first_apt_install.sh
@@ -1,41 +1,41 @@
-sudo apt clean
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get full-upgrade
-sudo  apt-get dist-upgrade
-sudo apt-get install maltego
-sudo apt-get install metasploit-framework
-sudo apt-get install burpsuite
-sudo apt-get install wireshark
-sudo apt-get install aircrack-ng 
-sudo apt-get install beef-xss
-sudo apt-get install nikto
-sudo apt-get install oneko
-sudo apt-get install neofetch
-sudo apt-get install lolcat
-sudo apt-get install fonts-nanum
-update-locale LANG=ko_KR.UTF-8
-sudo apt install kali-tools-sniffing-spoofing
-sudo apt install kali-desktop-core
-sudo apt install kali-linux-core
-sudo apt install kali-linux-default
-sudo apt install kali-tools-crypto-stego
-sudo apt install kali-tools-detect
-sudo apt install kali-tools-exploitation
-sudo apt install kali-tools-forensics
-sudo apt install kali-tools-fuzzing
-sudo apt install kali-tools-hardware
-sudo apt install kali-tools-identify
-sudo apt install kali-tools-information-gathering
-sudo apt install kali-tools-passwords
-sudo apt install kali-tools-post-exploitation
-sudo apt install kali-tools-reverse-engineering
-sudo apt install kali-tools-rfid
-sudo apt install kali-tools-top10
-sudo apt install kali-tools-voip
-sudo apt install kali-tools-vulnerability
-sudo apt install kali-tools-web
-sudo apt install kali-tools-wireless
-sudo apt install kali-tools-bluetooth
-sudo apt install kali-tools-802-11
-echo end!
+sudo apt clean -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y
+sudo  apt-get dist-upgrade -y
+sudo apt-get install maltego -y
+sudo apt-get install metasploit-framework -y
+sudo apt-get install burpsuite -y
+sudo apt-get install wireshark -y
+sudo apt-get install aircrack-ng -y 
+sudo apt-get install beef-xss -y
+sudo apt-get install nikto -y
+sudo apt-get install oneko -y
+sudo apt-get install neofetch -y
+sudo apt-get install lolcat -y
+sudo apt-get install fonts-nanum -y
+update-locale LANG=en_EN.UTF-8
+sudo apt install kali-tools-sniffing-spoofing -y
+sudo apt install kali-desktop-core -y
+sudo apt install kali-linux-core -y
+sudo apt install kali-linux-default -y
+sudo apt install kali-tools-crypto-stego -y
+sudo apt install kali-tools-detect -y
+sudo apt install kali-tools-exploitation -y
+sudo apt install kali-tools-forensics -y
+sudo apt install kali-tools-fuzzing -y
+sudo apt install kali-tools-hardware -y
+sudo apt install kali-tools-identify -y
+sudo apt install kali-tools-information-gathering -y
+sudo apt install kali-tools-passwords -y
+sudo apt install kali-tools-post-exploitation -y
+sudo apt install kali-tools-reverse-engineering -y
+sudo apt install kali-tools-rfid -y
+sudo apt install kali-tools-top10 -y
+sudo apt install kali-tools-voip -y
+sudo apt install kali-tools-vulnerability -y
+sudo apt install kali-tools-web -y
+sudo apt install kali-tools-wireless -y
+sudo apt install kali-tools-bluetooth -y
+sudo apt install kali-tools-802-11 -y
+echo end! 
